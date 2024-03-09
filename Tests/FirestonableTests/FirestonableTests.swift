3e46ea8abf65d5da89041472962d1d2582e11cf5
HammerCoded <scuff.04.aglow@icloud.com>
2024-03-09 08:22:08 +0100

    Initial Commit

---
+++ Tests/FirestonableTests/FirestonableTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Firestonable
+
+final class FirestonableTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
